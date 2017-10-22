0f9ac8d7b0b8e8726fc8b9f3d706ed6a7bfb06d4
Hemang Shah <hemangshah.in@gmail.com>
2017-10-22 12:06:17 +0530

    Updated Usage Example

--- HHFloatingView/BaseViewController.swift
+++ HHFloatingView/BaseViewController.swift
@@ -24,51 +24,41 @@ class BaseViewController: UIViewController {
     
     //MARK: Add Floating View.
     internal func addFloatingView() {
-        
-        let fv = self.floatingView
-        fv.delegate = self
-        fv.datasource = self
-        self.view.addSubview(floatingView)
-        
-        //Add Image Icons
-        self.floatingViewOptionsImages.append(#imageLiteral(resourceName: "icon-ufo"))
-        self.floatingViewOptionsImages.append(#imageLiteral(resourceName: "icon-gift"))
-        self.floatingViewOptionsImages.append(#imageLiteral(resourceName: "icon-megaphone"))
-        self.floatingViewOptionsImages.append(#imageLiteral(resourceName: "icon-rocket"))
-        self.floatingViewOptionsImages.append(#imageLiteral(resourceName: "icon-umbrella"))
-        
-        //Add Colors
-        self.floatingViewOptionsColors.append(UIColor.red.withAlphaComponent(0.5))
-        self.floatingViewOptionsColors.append(UIColor.purple.withAlphaComponent(0.5))
-        self.floatingViewOptionsColors.append(UIColor.blue.withAlphaComponent(0.5))
-        self.floatingViewOptionsColors.append(UIColor.cyan.withAlphaComponent(0.5))
-        self.floatingViewOptionsColors.append(UIColor.magenta.withAlphaComponent(0.5))
-        
+        self.floatingView.delegate = self
+        self.floatingView.datasource = self
+        self.view.addSubview(self.floatingView)
         self.floatingView.reload()
     }
 }
 
+//MARK: HHFloatingViewDatasource
 extension BaseViewController: HHFloatingViewDatasource {
     func floatingViewConfiguration(floatingView: HHFloatingView) -> HHFloatingViewConfiguration {
         let configure = HHFloatingViewConfiguration.init()
-        configure.optionColors = self.floatingViewOptionsColors
-        configure.optionImages = self.floatingViewOptionsImages
-        configure.handlerImage = #imageLiteral(resourceName: "icon-bird")
-        configure.handlerColor = UIColor.blue.withAlphaComponent(0.5)
+        configure.animationTimerDuration = 0.3
+        configure.internalAnimationTimerDuration = 0.2
         configure.position = .top
         configure.numberOfOptions = 5
         configure.handlerSize = CGSize.init(width: 90.0, height: 90.0)
         configure.optionsSize = CGSize.init(width: 70.0, height: 70.0)
         configure.initialMargin = 20.0
         configure.internalMargin = 10.0
-        configure.animationTimerDuration = 0.3
-        configure.internalAnimationTimerDuration = 0.2
+        configure.handlerImage = #imageLiteral(resourceName: "icon-bird")
+        configure.handlerColor = UIColor.blue.withAlphaComponent(0.5)
+        configure.optionImages = [#imageLiteral(resourceName: "icon-ufo"), #imageLiteral(resourceName: "icon-gift"), #imageLiteral(resourceName: "icon-megaphone"), #imageLiteral(resourceName: "icon-rocket"), #imageLiteral(resourceName: "icon-umbrella")]
+        configure.optionColors = [UIColor.red.withAlphaComponent(0.5),
+                                  UIColor.purple.withAlphaComponent(0.5),
+                                  UIColor.blue.withAlphaComponent(0.5),
+                                  UIColor.cyan.withAlphaComponent(0.5),
+                                  UIColor.magenta.withAlphaComponent(0.5)]
         return configure
     }
 }
 
+//MARK: HHFloatingViewDelegate
 extension BaseViewController: HHFloatingViewDelegate {
     func floatingView(floatingView: HHFloatingView, tappedAtIndex index: Int) {
+        print("HHFloatingView: Button Selected: \(index)")
         self.floatingView.close()
     }
 }
